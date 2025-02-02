264705be82fb8d7bcaaf8b0118b610a7c78fa5f9
Mritunjay Patel <97620757+Mrpatel2319@users.noreply.github.com>
2025-02-02 14:46:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function updateSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const sizeInfo = document.getElementById('sizeInfo').querySelector('h1');
+    sizeInfo.textContent = `Width: ${width} and Height: ${height}`;
+}
+
+window.addEventListener('resize', updateSize);
+
+updateSize();  // Initial size update
